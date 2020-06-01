2c669ba59add35cdc738f4b594fe3792c9c22793
ramprits <rampritsahani@outlook.in>
2020-06-01 11:37:08 +0530

    adding logger

--- src/index.js
+++ src/index.js
@@ -1,32 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux'
-import ReduxThunk from 'redux-thunk'
-import * as serviceWorker from './serviceWorker';
-import axios from 'axios';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import ReduxThunk from "redux-thunk";
+import ReduxLogger from "redux-logger";
+import * as serviceWorker from "./serviceWorker";
+import axios from "axios";
 
-import App from './components/App';
-import config from './config';
-import reducers from './reducers';
+import App from "./components/App";
+import config from "./config";
+import reducers from "./reducers";
 
 axios.defaults.baseURL = config.baseURLApi;
-axios.defaults.headers.common['Content-Type'] = "application/json";
-const token = localStorage.getItem('token');
+axios.defaults.headers.common["Content-Type"] = "application/json";
+const token = localStorage.getItem("token");
 if (token) {
-    axios.defaults.headers.common['Authorization'] = "Bearer " + token;
+  axios.defaults.headers.common["Authorization"] = "Bearer " + token;
 }
-
-const store = createStore(
-  reducers,
-  applyMiddleware(ReduxThunk)
-);
+const middlewares = [ReduxThunk];
+if (process.NODE_ENV === "development") {
+  middlewares.push(ReduxLogger);
+}
+const store = createStore(reducers, applyMiddleware(...middlewares));
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
